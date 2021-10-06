2f43375c7a40ddf6fe4864e7e85f634fcff172d7
Gagan139896 <gagandeep.singh@crochetech.com>
2021-10-06 10:38:42 +0530

    Exercise_01/02 commit

--- Test_script/Test_01.py
+++ Test_script/Test_01.py
@@ -1,4 +1,3 @@
-import imghdr
 import smtplib
 from email.message import EmailMessage
 from lib2to3.pgen2 import driver
@@ -8,65 +7,65 @@ from fpdf import FPDF
 
 PDFtext = []
 # Chrome executable path
-#driver=webdriver.Chrome(executable_path='C:\Python39\Lib\site-packages\selenium\webdriver\chrome\chromedriver')
+driver=webdriver.Chrome(executable_path='C:\Python39\Lib\site-packages\selenium\webdriver\chrome\chromedriver')
 
 #To maximize window
-# driver.maximize_window()
-# driver.implicitly_wait(10)
-# PDFtext.append("Window Maximized")
+driver.maximize_window()
+driver.implicitly_wait(10)
+PDFtext.append("Window Maximized")
 
 #To open URL
-# driver.get("https://beneficienttest.appiancloud.com/suite/")
-# PDFtext.append("Website Opened")
-# time.sleep(2)
+driver.get("https://beneficienttest.appiancloud.com/suite/")
+PDFtext.append("Website Opened")
+time.sleep(2)
 
 #To enter username
-# username=driver.find_element_by_xpath("//div[@class='login_box_inner']/form[2]/div[1]/div/input")
-# print(username.is_displayed()) #returns true/false based element status
-# print(username.is_enabled()) #return true/false
-# username.send_keys("neeraj.kumar")
-# PDFtext.append("Username Entered Successfully")
-# time.sleep(3)
+username=driver.find_element_by_xpath("//div[@class='login_box_inner']/form[2]/div[1]/div/input")
+print(username.is_displayed()) #returns true/false based element status
+print(username.is_enabled()) #return true/false
+username.send_keys("neeraj.kumar")
+PDFtext.append("Username Entered Successfully")
+time.sleep(3)
 
 #To enter password
-# password=driver.find_element_by_xpath("//div[@class='login_box_inner']/form[2]/div[2]/div/input")
-# print(password.is_displayed()) #returns true/false based element status
-# print(password.is_enabled()) #return true/false
-# password.send_keys("Crochet@786")
+password=driver.find_element_by_xpath("//div[@class='login_box_inner']/form[2]/div[2]/div/input")
+print(password.is_displayed()) #returns true/false based element status
+print(password.is_enabled()) #return true/false
+password.send_keys("Crochet@786")
 
 #PDFtext.append("Password Entered Successfully")
 
-# driver.find_element_by_xpath("//div[@class='login_box_inner']/form[2]/div[4]/div/div[2]/input").click()
-# PDFtext.append("Login successfully")
-# time.sleep(2)
+driver.find_element_by_xpath("//div[@class='login_box_inner']/form[2]/div[4]/div/div[2]/input").click()
+PDFtext.append("Login successfully")
+time.sleep(2)
 
 # print(str(len(PDFtext)))
-# pdf = FPDF()
-# pdf.add_page()
-# pdf.set_font("helvetica", 'B' , size=15)
-# pdf.image("C:/Users/Gagan/PycharmProjects/Exercise_01/Test_script/PDF_01/Ben.png", x = 150, y = 5, w = 40, h = 20, type = 'PNG', link = '')
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("helvetica", 'B' , size=15)
+pdf.image("C:/Users/Gagan/PycharmProjects/Exercise_01/Test_script/PDF_01/Ben.png", x = 150, y = 5, w = 40, h = 20, type = 'PNG', link = '')
 
 #To Open Each Module
-# for x in range (1,6):
-#     driver.find_element_by_xpath("//ul[@class='SiteHeaderLayout---site_nav']/li["+str( x )+"]").click()
-#     for iat3 in range(1000):
-#         try:
-#             bool = driver.find_element_by_xpath(
-#                 "//div[@id='appian-working-indicator-hidden']").is_enabled()
-#         except Exception:
-#             time.sleep(1)
-#             break
-#     time.sleep(10)
-#     get_title = driver.title
-#     print(get_title)
-#     PDFtext.append(get_title)
-#
-# print(len(PDFtext))
-# for i in range (len(PDFtext)):
-#     print(PDFtext[i])
-#     pdf.cell(10, 15, txt =PDFtext[i], ln =10, align ="L")
-# pdf.output("C:/Users/Gagan/PycharmProjects/Exercise_01/Test_script/PDF_01/Exercise_02.pdf")
-# time.sleep(10)
+for x in range (1,6):
+    driver.find_element_by_xpath("//ul[@class='SiteHeaderLayout---site_nav']/li["+str( x )+"]").click()
+    for iat3 in range(1000):
+        try:
+            bool = driver.find_element_by_xpath(
+                "//div[@id='appian-working-indicator-hidden']").is_enabled()
+        except Exception:
+            time.sleep(1)
+            break
+    time.sleep(10)
+    get_title = driver.title
+    print(get_title)
+    PDFtext.append(get_title)
+
+print(len(PDFtext))
+for i in range (len(PDFtext)):
+    print(PDFtext[i])
+    pdf.cell(10, 15, txt =PDFtext[i], ln =10, align ="L")
+pdf.output("C:/Users/Gagan/PycharmProjects/Exercise_01/Test_script/PDF_01/Exercise_02.pdf")
+time.sleep(10)
 
 # To Send Email
 msg=EmailMessage()
